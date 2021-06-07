f5e12d319a1d4bac9a43601a9d43a24939f0771d
Nicolas DUBIEN <github@dubien.org>
2021-06-08 00:59:14 +0200

    Better start for profile

--- profile.js
+++ profile.js
@@ -1,11 +1,9 @@
 import fc from "fc-99.99.99";
 
 function runIt() {
-  const arb1 = fc.string();
-  const arb2 = fc.integer();
-  const arb3 = fc.double();
+  const arbs = [fc.array(fc.integer(), { maxLength: 100_000 })];
   for (let idx = 0; idx !== 1000; ++idx) {
-    fc.assert(fc.property(arb1, arb2, arb3, (_unused) => true));
+    fc.assert(fc.property(...arbs, (_unused) => true));
   }
 }
 
